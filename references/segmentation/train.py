28037972850a0d5183dcb38c1e825cfb3275e005
Yuje Lee <yuje.lee@nota.ai>
2023-04-24 02:26:55 +0000

    Revert "fix: conditional import PolynomialLR"
    This reverts commit 77e942861bfd4cd874e08043124332c7f204f83c.

--- references/segmentation/train.py
+++ references/segmentation/train.py
@@ -10,60 +10,9 @@ import torchvision
 import utils
 from coco_utils import get_coco
 from torch import nn
+from torch.optim.lr_scheduler import PolynomialLR
 from torchvision.transforms import functional as F, InterpolationMode
 
-if torch.__version__ >= (1,13,0):
-    from torch.optim.lr_scheduler import PolynomialLR
-else:
-    from torch.optim.lr_scheduler import _LRScheduler
-    class PolynomialLR(_LRScheduler):
-        """Decays the learning rate of each parameter group using a polynomial function
-        in the given total_iters. When last_epoch=-1, sets initial lr as lr.
-        Args:
-            optimizer (Optimizer): Wrapped optimizer.
-            total_iters (int): The number of steps that the scheduler decays the learning rate. Default: 5.
-            power (int): The power of the polynomial. Default: 1.0.
-            verbose (bool): If ``True``, prints a message to stdout for
-                each update. Default: ``False``.
-        Example:
-            >>> # Assuming optimizer uses lr = 0.001 for all groups
-            >>> # lr = 0.001     if epoch == 0
-            >>> # lr = 0.00075   if epoch == 1
-            >>> # lr = 0.00050   if epoch == 2
-            >>> # lr = 0.00025   if epoch == 3
-            >>> # lr = 0.0       if epoch >= 4
-            >>> # xdoctest: +SKIP("undefined vars")
-            >>> scheduler = PolynomialLR(self.opt, total_iters=4, power=1.0)
-            >>> for epoch in range(100):
-            >>>     train(...)
-            >>>     validate(...)
-            >>>     scheduler.step()
-        """
-        def __init__(self, optimizer, total_iters=5, power=1.0, last_epoch=-1, verbose=False):
-            self.total_iters = total_iters
-            self.power = power
-            super().__init__(optimizer, last_epoch, verbose)
-
-        def get_lr(self):
-            if not self._get_lr_called_within_step:
-                warnings.warn("To get the last learning rate computed by the scheduler, "
-                            "please use `get_last_lr()`.", UserWarning)
-
-            if self.last_epoch == 0 or self.last_epoch > self.total_iters:
-                return [group["lr"] for group in self.optimizer.param_groups]
-
-            decay_factor = ((1.0 - self.last_epoch / self.total_iters) / (1.0 - (self.last_epoch - 1) / self.total_iters)) ** self.power
-            return [group["lr"] * decay_factor for group in self.optimizer.param_groups]
-
-        def _get_closed_form_lr(self):
-            return [
-                (
-                    base_lr * (1.0 - min(self.total_iters, self.last_epoch) / self.total_iters) ** self.power
-                )
-                for base_lr in self.base_lrs
-            ]
-
-from torch.optim.lr_scheduler import PolynomialLR
 
 def get_dataset(dir_path, name, image_set, transform):
     def sbd(*args, **kwargs):
@@ -224,29 +173,13 @@ def main(args):
         model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
         model_without_ddp = model.module
 
-
-        backbone = []
-        classifier = []
-        aux_classifier = []
-
-        for name, params in model_without_ddp.named_parameters():
-            if 'backbone' in name:
-                if params.requires_grad:
-                    backbone.append(params)
-            elif 'aux' in name:
-                if params.requires_grad:
-                    aux_classifier.append(params)
-            elif 'classifier' in name:
-                if params.requires_grad:
-                    classifier.append(params)
-        params_to_optimize = [
-            {"params": backbone},
-            {"params": classifier},
-        ]
-        if args.aux_loss:
-            params_to_optimize.append({"params":aux_classifier, "lr":args.lr*10})
-
-    
+    params_to_optimize = [
+        {"params": [p for p in model_without_ddp.backbone.parameters() if p.requires_grad]},
+        {"params": [p for p in model_without_ddp.classifier.parameters() if p.requires_grad]},
+    ]
+    if args.aux_loss:
+        params = [p for p in model_without_ddp.aux_classifier.parameters() if p.requires_grad]
+        params_to_optimize.append({"params": params, "lr": args.lr * 10})
     optimizer = torch.optim.SGD(params_to_optimize, lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
 
     scaler = torch.cuda.amp.GradScaler() if args.amp else None
